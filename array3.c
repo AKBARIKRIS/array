0750b933f86bfb469a0634b220ef38a8f5fed4d5
AKBARIKRIS <137590850+AKBARIKRIS@users.noreply.github.com>
2023-07-14 04:31:26 -0700

    Add files via upload

---
+++ array3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int ave,a[100],n,i,sum,array;
+	printf("enter the no of tearm=");
+	scanf("%d",&n);
+	printf("\n enter the no=");
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+		ave=sum/n;
+	}
+	printf("ave of %d no is =%d \n",n,ave);
+}
